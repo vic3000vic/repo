4202fb99a7cc91763e49aa350840bfb5870eacdf
vic3000vic <vic3000git@gmail.com>
2025-11-07 16:25:26 +0500

    index_001 and service_oo1

---
+++ index_001.php
@@ -0,0 +1,22 @@
+<?php
+//phpinfo();
+
+
+echo date (format: 'r')  . PHP_EOL;
+//echo '<pre>';
+echo '<br>';
+echo 'PHP version:  ' .  phpversion()  . PHP_EOL;
+echo '<br>';
+
+echo "Hello" . (php_sapi_name() === 'cli' ? PHP_EOL : '<br>') . "World";
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+
+echo "GOODBY" . (php_sapi_name() === 'cli' ? PHP_EOL : '<br>') . "World!";
+
+echo "index_oo1.php";
+echo "feature/new_br_010.php";
+
+
---
+++ service_001.php
@@ -0,0 +1,18 @@
+<?php
+//phpinfo();
+
+
+echo date (format: 'r')  . PHP_EOL;
+//echo '<pre>';
+echo '<br>';
+echo 'PHP version:  ' .  phpversion()  . PHP_EOL;
+echo '<br>';
+
+echo "Hello" . (php_sapi_name() === 'cli' ? PHP_EOL : '<br>') . "World";
+echo '<br>';
+echo '<br>';
+
+echo "GOODBY" . (php_sapi_name() === 'cli' ? PHP_EOL : '<br>') . "World!";
+
+echo "service_oo1.php";
+echo "feature/new_br_010.php";
